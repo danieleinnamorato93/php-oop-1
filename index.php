830bce879992d06580763fd3d4cdb5ab657c425b
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-09 15:59:31 +0200

    constructor added

--- index.php
+++ index.php
@@ -3,9 +3,22 @@ class Movie {
     public $title;
     public $year;
     public $genre;
-}
-$eraserhead = new Movie();
-$alien = new Movie();
-$her = new Movie();
 
+    function __construct($_title, $_year , $_genre)
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->genre = $_genre;
+    }
+}
+$eraserhead = new Movie("Eraserhead", "1977","grottesco");
+
+$alien = new Movie("Alien","1979","fantascienza");
+
+$her = new Movie("Her", "2013","fantascienza");
+
+
+//var_dump($eraserhead);
+//var_dump($alien);
+//var_dump($her);
 ?>
